6bf97d9472a7edc5cd631de35eef98bb8a19fa73
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-16 20:44:19 +0400

    feature/pretter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './constants/Footer';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./constants/Footer";
 
 const App = () => {
   return (
-    <div className='w-full overflow-hidden bg-primary'>
-      <div className='flex flex-col items-center justify-center px-6 sm:px-16'>
-        <div className='max-w-[1280px] w-full'>
+    <div className="w-full overflow-hidden bg-primary">
+      <div className="flex flex-col items-center justify-center px-6 sm:px-16">
+        <div className="w-full max-w-[1280px]">
           <Navbar />
         </div>
 
-        <div className='flex items-center justify-center px-6 bg-primary sm:px-16 w-full'>
-          <div className='max-w-[1280px] w-full '>
+        <div className="flex w-full items-center justify-center bg-primary px-6 sm:px-16">
+          <div className="w-full max-w-[1280px]">
             <Footer />
           </div>
         </div>
--- src/constants/Footer.jsx
+++ src/constants/Footer.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import { logo } from '../assets';
-import { footerLinks } from '../constants/index';
-import { socialMedia } from '../constants/index';
+import { logo } from "../assets";
+import { footerLinks } from "../constants/index";
+import { socialMedia } from "../constants/index";
 
 const Footer = () => {
   return (
-    <footer className='flex justify-center items-center py-6 sm:py-16 flex-col'>
+    <footer className="flex flex-col items-center justify-center py-6 sm:py-16">
       {/* top side */}
-      <div className='flex justify-center items-start md:flex-row mb-8 w-full'>
+      <div className="mb-8 flex w-full items-start justify-center md:flex-row">
         {/* left side */}
-        <div className='flex-[1] flex flex-col justify-start mr-10 '>
+        <div className="mr-10 flex flex-[1] flex-col justify-start">
           <img
             src={logo}
-            alt='bank_logo'
-            className='w-[266px] h-[72.14px] object-contain *: '
+            alt="bank_logo"
+            className="*: h-[72.14px] w-[266px] object-contain"
           />
-          <p className='font-poppins font-normal text-dimWhite leading-[30.8px]  mt-4 max-w-[312px]  '>
+          <p className="mt-4 max-w-[312px] font-poppins font-normal leading-[30.8px] text-dimWhite">
             A new way to make the payments easy, reliable and secure.
           </p>
         </div>
         {/* right side */}
-        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10 '>
+        <div className="mt-10 flex w-full flex-[1.5] flex-row flex-wrap justify-between md:mt-0">
           {footerLinks.map((footerLink) => (
             <div
               key={footerLink.title}
-              className='felx fel-col ss:my-0 my-4 min-w-[150px]  '
+              className="felx fel-col my-4 min-w-[150px] ss:my-0"
             >
-              <h4 className='font-poppins font-medium text-[18px] leading-[28px] text-[white] '>
+              <h4 className="font-poppins text-[18px] font-medium leading-[28px] text-[white]">
                 {footerLink.title}
               </h4>
-              <ul className='list-none mt-4 gap-4 flex flex-col'>
+              <ul className="mt-4 flex list-none flex-col gap-4">
                 {footerLink.links.map((link) => (
                   <li
                     key={link.name}
-                    className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite cursor-pointer'
+                    className="cursor-pointer font-poppins text-[16px] font-normal leading-[24px] text-dimWhite"
                   >
                     <a href={link.link}>{link.name}</a>
                   </li>
@@ -48,17 +48,17 @@ const Footer = () => {
 
       <div />
 
-      <div className='w-full flex justify-between items-center flex-col md:flex-row pt-6 border-t-[1px] border-t-[#3f3e45] '>
-        <p className='font-poppins font-normal text-normal text-[White] text-[16px] leading-[27.8px]  mt-4 max-w-[312px]'>
+      <div className="flex w-full flex-col items-center justify-between border-t-[1px] border-t-[#3f3e45] pt-6 md:flex-row">
+        <p className="text-normal mt-4 max-w-[312px] font-poppins text-[16px] font-normal leading-[27.8px] text-[White]">
           Copyright c 2021 HooBank. All Rights Reserved.
         </p>
-        <div className='felx felx-row mt-6 md:mt-0 gap-[30px] '>
+        <div className="felx felx-row mt-6 gap-[30px] md:mt-0">
           {socialMedia.map((social) => (
             <img
               key={social.id}
               src={social.icon}
               alt={social.id}
-              className='w-[40px] h-[21px] object-contain cursor-pointer  '
+              className="h-[21px] w-[40px] cursor-pointer object-contain"
               onClick={() => window.open(social.link)}
             />
           ))}
